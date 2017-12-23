1c0f4c41a72c63b9deabea311c1b16cc00669f4b
ДосмаиловМ <@224_5_>
2017-12-23 14:08:12 +0600

    Add project files.

---
+++ ConsoleApplication9/Source.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "rus");
+    /*int bact = 1, i, time;
+	cout << "введите врем€: " << endl;
+	cin >> time;
+	for (i = 1; i <= time; i++)
+	{
+		bact *= 2;
+	}
+	cout << "Ѕактерий в момент времени " << time << " равно " << bact << endl;*/
+
+	/*int n, i, sum;*/
+
+	/*do
+	{
+		cout << "¬ведите последовательность чисел не провосход€щего n " << endl;
+		cin >> n;
+	}
+	while ((i % 1 == 0) && (i%i = 0)
+	{
+		cout << "ѕоследовательность простых чисел: " << n << endl;
+	}*/
+
+	///*int i;
+	//float A, B;
+	//cout << "¬ведите сумму A, ежемес€чную стипендию студента: " << endl;
+	//cin >> A;
+	//cout << "¬ведите сумму B, расход студента в мес€ц: " << endl;
+	//cin >> B;
+	//for (i = 1; i <= 10; i++)
+	//{
+	//	A += A;
+	//	B = B + B + (0.03*B);
+	//}
+	//cout << "ќбща€ стипенди€: " << A << endl;
+	//cout << "ќбщие расходы: " << B << endl;*/
+	////cout << "≈му не хватает суммы: " << B - A << endl;
+
+	/*int i;
+	float S = 24;
+	cout << "—умма в 1826 году равна: " << S << endl;
+
+	for (i = 1826; i <= 2017; i++)
+	{
+		S += (0.06*S);
+	}
+	cout << "—умма в насто€щий момент была бы равна: " << S << endl;*/
+
+}
